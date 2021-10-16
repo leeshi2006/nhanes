7be2d46e4894ae0560d0d56f385d1be721e9367f
MA Jian <majian1979@qq.com>
2021-10-16 15:06:11 +0800

    Update nhanes-lab1.r

--- nhanes-lab1.r
+++ nhanes-lab1.r
@@ -2,14 +2,15 @@ library(copent)
 library(lattice)
 
 load("lab1.rdata")
+n = dim(csv1)[2]
 
 mxce1 = matrix(0, n, n)
-l = 1000
+len = 1000
 for (i in 1:n){
   for(j in i:n){
-    data1 = csv1[1:l,c(i,j)]
-    data1[,1] = data1[,1] + max(abs(data1[,1])) * 0.00005 * runif(l)
-    data1[,2] = data1[,2] + max(abs(data1[,2])) * 0.00005 * runif(l)
+    data1 = csv1[1:len,c(i,j)]
+    data1[,1] = data1[,1] + max(abs(data1[,1])) * 0.00005 * runif(len)
+    data1[,2] = data1[,2] + max(abs(data1[,2])) * 0.00005 * runif(len)
     mxce1[i,j] = copent(data1)
     mxce1[j,i] = mxce1[i,j]
     str1 = paste("(",i,",",j,")")
